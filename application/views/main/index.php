ca41ec9c866726d93b41bdab8eb56153f33fb38f
edo masaru <masaruedo@yahoo.com>
2015-09-28 03:34:41 +0700

    Add extra small grid to the form

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -7,20 +7,20 @@
 			<div class="panel-body">						
 				<?php echo form_open("", array("class" => "form-horizontal")) ?>				
 					<div class="form-group">
-						<label for="text" class="col-md-3">Width</label>
-						<div class="col-md-3">
+						<label for="text" class="col-md-3 col-xs-3">Width</label>
+						<div class="col-md-3 col-xs-3">
 							<input type="text" name="image_width" value="" id="image_width" class="form-control">
 						</div>						
 					</div>					
 					<div class="form-group">
-						<label for="text" class="col-md-3">Height</label>
-						<div class="col-md-3">
+						<label for="text" class="col-md-3 col-xs-3">Height</label>
+						<div class="col-md-3 col-xs-3">
 							<input type="text" name="image_height" value="<?php echo set_value("image_height") ?>" id="image_height" class="form-control">
 						</div>						
 					</div>					
 					<div class="form-group">
-						<label for="text" class="col-md-3">Image Type</label>
-						<div class="col-md-3">
+						<label for="text" class="col-md-3 col-xs-3">Image Type</label>
+						<div class="col-md-3 col-xs-3">
 							<?php $types = array(
 								'.jpg' => "JPG",
 								'.png' => "PNG",
@@ -30,21 +30,21 @@
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="text" class="col-md-3">Image Text</label>
-						<div class="col-md-9">
+						<label for="text" class="col-md-3 col-xs-3">Image Text</label>
+						<div class="col-md-9 col-xs-9">
 							<input type="text" name="image_text" value="" id="image_text" class="form-control">
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="image_count" class="col-md-3">How many?</label>
-						<div class="col-md-3">
+						<label for="image_count" class="col-md-3 col-xs-3">How many?</label>
+						<div class="col-md-3 col-xs-3">
 							<?php foreach (range(1, 20) as $i) { $number[$i] = $i; } ?>
 							<?php echo form_dropdown("image_count", $number, "", 'class="form-control"') ?>
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="image_count" class="col-md-3">Provider</label>
-						<div class="col-md-5">
+						<label for="image_count" class="col-md-3 col-xs-3">Provider</label>
+						<div class="col-md-5 col-xs-5">
 							<?php $provider = array(
 								"http://placehold.it" => "http://placehold.it",
 								"http://dummyimage.com" => "http://dummyimage.com"
@@ -54,7 +54,7 @@
 					</div>
 					<hr>
 					<div class="form-group">
-						<div class="col-md-offset-3 col-md-5">
+						<div class="col-md-offset-3 col-md-5 col-xs-offset-3 col-xs-5">
 							<button type="submit" class="btn btn-primary">Generate</button>
 						</div>
 					</div>
